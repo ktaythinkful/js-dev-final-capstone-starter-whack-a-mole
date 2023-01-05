35e51d45c155f93c5d815f409ed23c2a1e7da781
ktaythinkful <112444175+ktaythinkful@users.noreply.github.com>
2023-01-04 22:03:27 -0600

    Update index.js
    Update updateScore function, clearScore function, updateTimer function, startTimer function.

--- src/index.js
+++ src/index.js
@@ -157,7 +157,8 @@ function toggleVisibility(hole){
 *
 */
 function updateScore() {
-  // TODO: Write your code here
+  points++
+  score.textContent = points
 
   return points;
 }
@@ -170,9 +171,8 @@ function updateScore() {
 *
 */
 function clearScore() {
-  // TODO: Write your code here
-  // points = 0;
-  // score.textContent = points;
+  points = 0;
+  score.textContent = points;
   return points;
 }
 
@@ -182,9 +182,10 @@ function clearScore() {
 *
 */
 function updateTimer() {
-  // TODO: Write your code here.
-  // hint: this code is provided to you in the instructions.
-  
+  if (time > 0){
+    time -= 1;
+    timerDisplay.textContent = time;
+  }
   return time;
 }
 
@@ -195,8 +196,7 @@ function updateTimer() {
 *
 */
 function startTimer() {
-  // TODO: Write your code here
-  // timer = setInterval(updateTimer, 1000);
+  timer = setInterval(updateTimer, 1000)
   return timer;
 }
 
