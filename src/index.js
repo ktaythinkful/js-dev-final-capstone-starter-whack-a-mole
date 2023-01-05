11ee9012c777ff9f24d1999ac9bdc9a93cc58e2e
ktaythinkful <112444175+ktaythinkful@users.noreply.github.com>
2023-01-04 21:58:49 -0600

    Update index.js
    Update showUp function, correct and beautify chooseHole function, update showAndHide function, update toggleVisibility function.

--- src/index.js
+++ src/index.js
@@ -67,7 +67,8 @@ function chooseHole(holes) {
   const hole = holes[index]
   if (hole === lastHole) {
      return chooseHole(holes)
-  } else (lastHole = hole) {
+  } else {
+     lastHole = hole
      return hole;
   }
 
@@ -107,8 +108,8 @@ function gameOver() {
 *
 */
 function showUp() {
-  let delay = 0; // TODO: Update so that it uses setDelay()
-  const hole = 0;  // TODO: Update so that it use chooseHole()
+  let delay = setDelay(difficulty) // TODO: Update so that it uses setDelay()
+  const hole = chooseHole(holes)  // TODO: Update so that it use chooseHole()
   return showAndHide(hole, delay);
 }
 
@@ -121,13 +122,15 @@ function showUp() {
 *
 */
 function showAndHide(hole, delay){
-  // TODO: call the toggleVisibility function so that it adds the 'show' class.
+  toggleVisibility(hole)
   
   const timeoutID = setTimeout(() => {
-    // TODO: call the toggleVisibility function so that it removes the 'show' class when the timer times out.
+   toggleVisibility(hole)
     
     gameOver();
-  }, 0); // TODO: change the setTimeout delay to the one provided as a parameter
+     
+  }, delay);
+
   return timeoutID;
 }
 
@@ -138,7 +141,7 @@ function showAndHide(hole, delay){
 *
 */
 function toggleVisibility(hole){
-  // TODO: add hole.classList.toggle so that it adds or removes the 'show' class.
+ hole.classList.toggle('show')
   
   return hole;
 }
