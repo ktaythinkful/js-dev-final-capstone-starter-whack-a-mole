471af22fd3bef2dce5dc9c48565b68a238455511
ktaythinkful <112444175+ktaythinkful@users.noreply.github.com>
2023-01-04 23:33:33 -0600

    Create script.js
    Audio work

---
+++ script.js
@@ -0,0 +1,19 @@
+const audioHit = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/hit.mp3?raw=true");
+const song = new Audio("https://github.com/gabrielsanchez/erddiagram/blob/main/molesong.mp3?raw=true");
+
+function playAudio(audioObject) {
+  audioObject.play();
+}
+
+function loopAudio(audioObject) {
+  audioObject.loop = true;
+  playAudio(audioObject);
+}
+
+function stopAudio(audioObject) {
+  audioObject.pause();
+}
+
+function play(){
+  playAudio(song);
+}
